4de846dee71102a75ccc08ef3287129802509833
samueldev45 <76446373+samueldev45@users.noreply.github.com>
2021-01-25 16:44:29 -0300

    Update main.py

--- example/main.py
+++ example/main.py
@@ -8,46 +8,43 @@ class Game(App):
         super().__init__(width, height, title, flags)
 
     def start(self):
-        self.gameScene = Scene(name=Game, render_physics=True)
+        self.gameScene = Scene("Game", True)
         # load images
         self.loadImage("cube", "cube.png")
         #---
 
         # entities
-        self.platform = Entity(name="Platform")
-        self.player = Entity(name="Player")
-        self.camera = Entity(name="Camera")
-        self.cube = Entity(name="Cube")
+        self.platform = Entity("Platform")
+        self.player = Entity("Player")
+        self.camera = Entity("Camera")
+        self.cube = Entity("Cube")
         #---
 
         # add components in entities
-        self.platform.add(Transform(position=Vector2(-96, 300)))
-        self.platform.add(BoxCollider(width=256, height=16))
+        self.platform.add(Transform(Vector2(-96, 300)))
+        self.platform.add(BoxCollider(256, 16))
 
-        self.player.add(Transform(position=Vector2(0, 0), scale=Vector2(64, 64)))
-        self.player.add(BoxCollider(width=64, height=64))
-        self.player.add(Rigidbody(app=self, use_gravity=False))
-        self.player.add(PlayerController(speed=5))
-        self.player.add(SpriteRenderer(app=self, image_name="cube"))
+        self.player.add(Transform(Vector2(0, 0), Vector2(64, 64)))
+        self.player.add(BoxCollider(64, 64))
+        self.player.add(Rigidbody(self, 5, 1, False))
+        self.player.add(PlayerController(5))
+        self.player.add(SpriteRenderer(self, "cube"))
 
-        self.cube.add(Transform(position=Vector2(-64, 0)))
-        self.cube.add(BoxCollider(width=32, height=32))
-        self.cube.add(Rigidbody(app=self, mass=10))
+        self.cube.add(Transform(Vector2(-64, 0)))
+        self.cube.add(BoxCollider(32, 32))
+        self.cube.add(Rigidbody(self, 2, 10, True))
 
-        self.camera.add(Transform(position=Vector2(0,0)))
-        self.camera.add(Camera(screen_width=800, screen_height=600, offset_x=-32, offset_y=-32))
+        self.camera.add(Transform(Vector2(0,0)))
+        self.camera.add(Camera(800, 600, -32, -32))
         #---
 
         self.camera.parent(self.player)
 
-        # adding entities in game scene
         self.gameScene.add(self.platform)
         self.gameScene.add(self.player)
         self.gameScene.add(self.cube)
         self.gameScene.add(self.camera)
-        #---
 
-        # setting camera to game scene and set scene in app
         self.gameScene.setCamera(self.camera)
         self.setScene(self.gameScene)
 
@@ -64,5 +61,5 @@ class Game(App):
     def end(self):
         print("end app")
 
-game = Game(width=800, height=600, title="Godity Engine", flags=0)
+game = Game(800, 600, "Godity Engine", 0)
 game.run()
